2f7dc1255dc05008380ac1c8cbd0c2aa7e46b757
rosstapson <ross.tapson@gmail.com>
2017-03-30 16:44:27 +0200

    just for moz.

---
+++ scratch.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+class Scratch extends Component {
+  render() {
+    return (
+      <View>
+        <Text>testing testing...</Text>
+      </View>
+    )
+  }
+}
+export default Scratch;
