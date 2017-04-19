10ad0656dc0d12a8e4489b679b6adb59863de69b
rosstapson <ross.tapson@gmail.com>
2017-04-19 09:01:34 +0200

    about to add camera. *genuflect*

--- assetListItem.js
+++ assetListItem.js
@@ -13,12 +13,18 @@ import constants from './constants';
 class AssetListItem extends Component {
   constructor(props) {
     super(props);
+    
     this.state={
       asset: this.props.asset,
       onDetailPress: this.props.onDetailPress,
-      onThumbnailPress: this.props.onThumbnailPress
+      onThumbnailPress: this.props.onThumbnailPress,
+      // onCameraPress: this.props.onCameraPress,
+      imagesAreAvailable: this.props.asset.imageUrls[0]
     }
     console.log(constants.IMG_API + this.state.asset.imageUrls[0]);
+  }
+  onCameraPress() {
+    alert("onCameraPress: " + this.props.asset.dnaCode)
   }
   render() {
     return(
@@ -30,13 +36,24 @@ class AssetListItem extends Component {
             <Text><Text style={styles.textBold}>Description: </Text>{this.props.asset.description}</Text>
           </TouchableOpacity>
         </View>
-        <View style={styles.assetSmallThumbnail}>
-        <TouchableOpacity onPress={() => {this.state.onThumbnailPress(this.props.asset.assetCode)}}>
-          <Image             
-            source={{uri: constants.IMG_API + this.state.asset.imageUrls[0]}}            
-            style={{width: 100, height: 100}}            
-          />
-          </TouchableOpacity>
+        <View style={styles.assetSmallThumbnail}>        
+          {this.state.imagesAreAvailable &&
+            <TouchableOpacity onPress={() => {this.state.onThumbnailPress(this.props.asset.assetCode)}}>
+              <Image             
+                source={{uri: constants.IMG_API + this.state.asset.imageUrls[0]}}            
+                style={{width: 100, height: 100}}            
+              />
+            </TouchableOpacity>
+          }
+          {!this.state.imagesAreAvailable &&
+            <TouchableOpacity onPress={this.onCameraPress.bind(this)}>
+              <Image             
+                source={require('./icons/ic_camera.png')}        
+                style={{width: 60, height: 60}}            
+              />               
+            </TouchableOpacity>
+          }
+          
         </View>
       </View>
     )
