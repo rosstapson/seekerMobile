4f3ad3f0ed999346b51eafdf504c73194a85a7a9
Ross Tapson <ross@Rosss-Mac.local>
2017-05-16 10:09:07 +0200

    index.ios.js and Info.plist

---
+++ index.ios.js
@@ -0,0 +1,81 @@
+
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet, Text, View, Navigator} from 'react-native';
+import { MenuContext } from 'react-native-menu';
+
+import Register from './register';
+import Login from './login';
+import Root from './root';
+import Home from './home';
+import RegistrationComplete from './registrationComplete';
+import Assets from './assets';
+import Account from './account';
+import MyCamera from './components/myCamera';
+import PicPreview from './components/picPreview';
+import ImageGallery from './imageGallery';
+import CaptureAsset from './captureAsset'
+
+// import Scratch from './scratch';
+import styles from './styles';
+
+export default class seekerMobile extends Component {
+  renderScene(route, navigator) {
+    if (route.name == 'root') {
+      return <Root navigator={navigator}/>
+    }
+    if (route.name == 'register') {
+      return <Register navigator={navigator}/>
+    }
+    if (route.name == 'login') {
+      return <Login navigator={navigator}/>
+    }
+    if (route.name == 'home') {
+      return <Home navigator={navigator} {... route.props}/>
+    }
+    if (route.name == 'registrationComplete') {
+      return <RegistrationComplete navigator={navigator}/>
+    }
+    if (route.name == 'assets') {
+      //console.log("routing assets");
+      return <Assets navigator={navigator}  {... route.props} />
+    }
+    if (route.name == 'myCamera') {
+      //console.log("routing myCamera");
+      return <MyCamera navigator={navigator}  {... route.props} />
+    }
+    if (route.name == 'picPreview') {
+      //console.log("routing picPreview");
+      return <PicPreview navigator={navigator} {...route.props} />
+    }
+    if(route.name == 'imageGallery') {
+      //console.log("routing image gallery");
+      return <ImageGallery navigator={navigator} {...route.props} />
+    }
+    if(route.name == 'capture') {
+      //console.log("routing capture asset");
+      return <CaptureAsset navigator={navigator} {...route.props} />
+    }
+    if(route.name == 'account') {
+      //console.log("routing capture asset");
+      return <Account navigator={navigator} {...route.props} />
+    }
+  }
+  render() {
+    return (
+      <MenuContext style={styles.container}>
+        <Navigator
+          initialRoute={{
+          name: "root"
+
+        }}
+          renderScene={this
+          .renderScene
+          .bind(this)}
+
+          />
+      </MenuContext>
+    );
+  }
+}
+
+AppRegistry.registerComponent('seekerMobile', () => seekerMobile);
